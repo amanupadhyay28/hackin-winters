80519abc7d42f0ddc29cb4ab2117c4d3e02384fe
Himanshu Dixit <himanshuharshdixit@gmail.com>
2022-11-13 19:30:14 +0530

    change host

--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from decouple import config
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=int(config("PORT")))
+    uvicorn.run(app, port=int(config("PORT")))
